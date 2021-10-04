4502d18e59f018c7fbe8d7a9113681501c68a020
booldook <booldook@gmail.com>
2021-10-04 13:24:22 +0900

    상품검색기

--- js/03.shop.js
+++ js/03.shop.js
@@ -1,13 +1,22 @@
 new Vue({
 	el: '#app',
 	data: {
-		products: []
+		products: [],
+		searchResult: [],
+		query: ''
 	},
 	async created() {
 		const { data } = await axios.get('../json/foods.json')
 		this.products = data
+		this.searchResult = data
 	},
 	methods: {
-
+		onReset(e) {
+			this.query = ''
+			this.searchResult = this.products
+		},
+		onSearch(e) {
+			this.searchResult = this.products.filter(v => v.title.includes(this.query))
+		}
 	}
 })
