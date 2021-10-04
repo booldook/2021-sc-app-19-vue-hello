2b0043796a5efd74941be57a56d7023b4c3aa254
booldook <booldook@gmail.com>
2021-10-04 12:33:13 +0900

    json

--- js/02.gallery.js
+++ js/02.gallery.js
@@ -1,13 +1,3 @@
-const imgs = [
-	{ id: 1, src: '../img/f1.jpg', title: '아이스크림' },
-	{ id: 2, src: '../img/f2.jpg', title: '연어 스테이크' },
-	{ id: 3, src: '../img/f3.jpg', title: '샌드위치' },
-	{ id: 4, src: '../img/f4.jpg', title: '스테이크' },
-	{ id: 5, src: '../img/f5.jpg', title: '와인' },
-	// { id: 6, src: '../img/f6.jpg', title: '체리' },
-	// { id: 7, src: '../img/f7.jpg', title: '크로상' },
-]
-
 new Vue({
 	el: '#app',
 	data: {
@@ -19,6 +9,7 @@ new Vue({
 		this.imgs = imgs
 		this.src = this.imgs[0].src
 		this.title = this.imgs[0].title
+		
 	},
 	methods: {
 		showImg(e) {
